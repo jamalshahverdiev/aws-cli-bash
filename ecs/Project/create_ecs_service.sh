18b8ed7c109842c6cb6287f0f74d40190934deae
Jamal Shahverdiev <jamal.shahverdiev@gmail.com>
2022-03-20 12:05:58 +0300

    Update create_ecs_service.sh

--- ecs/Project/create_ecs_service.sh
+++ ecs/Project/create_ecs_service.sh
@@ -2,13 +2,21 @@
 . ./libs/variables.sh
 . ./libs/ecs_functions.sh
 
-export ecs_svc_name='sandbox-site'
-export TD_ARN='ARN_OF_TASK_DEFINITION'
-export TG_ARN='ARN_OF_TARGET_GROUP'
-export TG_CONTAINER='sandbox-site'
-export SECURITY_GROUP_ID='SECURITY_GROUP_ID'
+SVC_TD_NAME=$1
+export TD_ARN=$(aws ecs list-task-definitions \
+    --region ${region_name} | jq -r '.taskDefinitionArns[]' | grep ${SVC_TD_NAME} | tail -n1)
+export TG_ARN=$(aws elbv2 describe-target-groups \
+    --region $region_name | jq -r '.TargetGroups[].TargetGroupArn' | grep $SVC_TD_NAME)
+export ECS_SVC_NAME=${SVC_TD_NAME}
+export TG_CONTAINER=${SVC_TD_NAME}
+export SECURITY_GROUP_ID=$(aws ec2 describe-security-groups \
+    --region ${region_name} \
+    --filters Name=group-name,Values=*${ecs_sg_name}* \
+    --query "SecurityGroups[*].{ID:GroupId}" \
+    --output text)
 
-create_ecs_svc ${region_name} ${cluster_name} ${ecs_svc_name} ${ecs_svc_temp_file_output}
+create_ecs_svc ${region_name} ${cluster_name} ${ECS_SVC_NAME} ${ecs_svc_temp_file_output}
+
 
 # Create a ECS Service and specifying a Capacity Provider:
 # aws ecs create-service \
